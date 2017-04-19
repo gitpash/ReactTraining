20b36aca41a95fac36bfd43503e3a0b7de734d8f
gitpash <arklogin@gmail.com>
2017-04-19 14:36:47 +0300

    add SFC

--- github-battleV2/app/components/popular.js
+++ github-battleV2/app/components/popular.js
@@ -1,37 +1,51 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-class Popular extends React.Component {
-        
-    constructor(props) {
-        super(props)
-        this.state = {
-            selectedLanguage: 'All'
-        }
-
-    this.updateLanguage = this.updateLanguage.bind(this) // for correct this binding in func invocation
-    }
-    // create new method to update state of languages
-    updateLanguage(lang) {
-        this.setState(() =>
-            ({selectedLanguage: lang})
-        )
-    }
-    render() {
-        const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
-        // console.log('before func invoke', this)
-        return(
-            <ul className="languages">
-                {languages.map(lang => 
-                    <li
-                        style={lang === this.state.selectedLanguage ? {color: '#d0021b'} : null }
-                        onClick={this.updateLanguage.bind(null, lang)}
-                        key={lang}
-                    >
-                        {lang} 
-                    </li>
-                , this)}
-            </ul>
-        )
-    }
+function SelectLanguage(props) {
+  const languages = ["All", "Javascript", "Ruby", "Java", "CSS", "Python"];
+  return (
+    <ul className="languages">
+      {languages.map(lang => (
+        <li
+          style={lang === props.selectedLanguage ? { color: "#d0021b" } : null}
+          onClick={props.onSelect.bind(null, lang)}
+          key={lang}
+        >
+          {lang}
+        </li>
+      ))}
+    </ul>
+  );
 }
-export default Popular
+
+SelectLanguage.PropTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
+};
+class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: "All"
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this); // for correct this binding in func invocation
+  }
+  // create new method to update state of languages
+  updateLanguage(lang) {
+    this.setState(() => ({ selectedLanguage: lang }));
+  }
+  render() {
+    // console.log('before func invoke', this)
+    return (
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
+        ;
+      </div>
+    );
+  }
+}
+export default Popular;
